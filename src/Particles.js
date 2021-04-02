69f40e421bc6f883893be05116c3786cc8f8ac33
Yury Petrov <yuratorn@gmail.com>
2021-04-02 22:55:46 +0300

    new dpi multiplier

--- src/Particles.js
+++ src/Particles.js
@@ -1,310 +1,347 @@
-import Renderer from "./Renderer.js"
-import { ParticleManager } from "./Particle.js"
-import Vector2 from "./Vector2.js"
-import Rectangle from "./Rectangle.js"
-import Line from "./Line.js"
-import QuadTree from "./QuadTree.js"
-import Circle from "./Circle.js"
-import Random from "./Random.js"
+import Renderer from './Renderer.js'
+import { ParticleManager } from './Particle.js'
+import Vector2 from './Vector2.js'
+import Rectangle from './Rectangle.js'
+import Line from './Line.js'
+import QuadTree from './QuadTree.js'
+import Circle from './Circle.js'
+import Random from './Random.js'
 
 export default class Particles {
-	constructor(canvas) {
-		this._canvas = canvas
+  constructor(canvas) {
+    this._canvas = canvas
 
-		this._ctx = this._canvas.getContext("2d")
+    this._ctx = this._canvas.getContext('2d')
 
-		this._particleManager = null
+    this._particleManager = null
 
-		this._renderer = null
+    this._dpiMultiplier = null
 
-		this._boundUpdate = this._update.bind(this)
+    this._renderer = null
 
-		this._isRunning = false
+    this._boundUpdate = this._update.bind(this)
 
-		this._ticks = 0
+    this._isRunning = false
 
-		this._deltas = new Array(200)
+    this._ticks = 0
 
-		this._debug = false
+    this._deltas = new Array(200)
 
-		this._boundary = null
+    this._debug = false
 
-		this._quadtree = null
+    this._boundary = null
 
-		this._resizeTimeout = null
-	}
+    this._quadtree = null
 
-	init(settings) {
-		this._settings = settings
+    this._resizeTimeout = null
+  }
 
-		this._particleManager = new ParticleManager()
+  init(settings) {
+    this._settings = settings
 
-		for (let a = 0; a < settings.particles.amount; a++) {
+    this._particleManager = new ParticleManager()
+
+    for (let a = 0; a < settings.particles.amount; a++) {
       const particle = this._particleManager.createParticle()
 
-			particle.position.set(
-				Random.intBetween(
-					-settings.particles.distanceToLink,
-					settings.renderer.width + settings.particles.distanceToLink
-				), 
-				Random.intBetween(
-					-settings.particles.distanceToLink,
-					settings.renderer.height + settings.particles.distanceToLink
-				)
-			)
+      particle.position.set(
+        Random.intBetween(
+          -settings.particles.distanceToLink,
+          settings.renderer.width + settings.particles.distanceToLink
+        ),
+        Random.intBetween(
+          -settings.particles.distanceToLink,
+          settings.renderer.height + settings.particles.distanceToLink
+        )
+      )
 
-			const velocity = new Vector2()
-			const alpha = 3
-			const beta = 4
-			switch(settings.particles.moveDirection) {
-				case 'top':
-					velocity.set(
-						Random.floatBetween(
-							-settings.particles.maxVelocity / alpha,
-							settings.particles.maxVelocity / alpha
-						),
-						Random.floatBetween(
-							-settings.particles.maxVelocity,
-							-settings.particles.maxVelocity / beta
-						),
-					)
-					break
-				case 'right':
-					velocity.set(
-						Random.floatBetween(
-							settings.particles.maxVelocity,
-							settings.particles.maxVelocity / beta
-						),
-						Random.floatBetween(
-							-settings.particles.maxVelocity / alpha,
-							settings.particles.maxVelocity / alpha
-						),
-					)
-					break
-				case 'bottom':
-					velocity.set(
-						Random.floatBetween(
-							-settings.particles.maxVelocity / alpha,
-							settings.particles.maxVelocity / alpha
-						),
-						Random.floatBetween(
-							settings.particles.maxVelocity,
-							settings.particles.maxVelocity / beta
-						),
-					)
-					break
-				case 'left':
-					velocity.set(
-						Random.floatBetween(
-							-settings.particles.maxVelocity,
-							-settings.particles.maxVelocity / beta
-						),
-						Random.floatBetween(
-							-settings.particles.maxVelocity / alpha,
-							settings.particles.maxVelocity / alpha
-						),
-					)
-					break
-				default:
-				case 'random':
-					velocity.set(
-						Random.floatBetween(
-							-settings.particles.maxVelocity,
-							settings.particles.maxVelocity
-						),
-						Random.floatBetween(
-							-settings.particles.maxVelocity,
-							settings.particles.maxVelocity
-						),
-					)
-					break
-			}
-			particle.velocity = velocity
+      const velocity = new Vector2()
+      const alpha = 3
+      const beta = 4
+      switch (settings.particles.moveDirection) {
+        case 'top':
+          velocity.set(
+            Random.floatBetween(
+              -settings.particles.maxVelocity / alpha,
+              settings.particles.maxVelocity / alpha
+            ),
+            Random.floatBetween(
+              -settings.particles.maxVelocity,
+              -settings.particles.maxVelocity / beta
+            )
+          )
+          break
+        case 'right':
+          velocity.set(
+            Random.floatBetween(
+              settings.particles.maxVelocity,
+              settings.particles.maxVelocity / beta
+            ),
+            Random.floatBetween(
+              -settings.particles.maxVelocity / alpha,
+              settings.particles.maxVelocity / alpha
+            )
+          )
+          break
+        case 'bottom':
+          velocity.set(
+            Random.floatBetween(
+              -settings.particles.maxVelocity / alpha,
+              settings.particles.maxVelocity / alpha
+            ),
+            Random.floatBetween(
+              settings.particles.maxVelocity,
+              settings.particles.maxVelocity / beta
+            )
+          )
+          break
+        case 'left':
+          velocity.set(
+            Random.floatBetween(
+              -settings.particles.maxVelocity,
+              -settings.particles.maxVelocity / beta
+            ),
+            Random.floatBetween(
+              -settings.particles.maxVelocity / alpha,
+              settings.particles.maxVelocity / alpha
+            )
+          )
+          break
+        default:
+        case 'random':
+          velocity.set(
+            Random.floatBetween(
+              -settings.particles.maxVelocity,
+              settings.particles.maxVelocity
+            ),
+            Random.floatBetween(
+              -settings.particles.maxVelocity,
+              settings.particles.maxVelocity
+            )
+          )
+          break
+      }
+      particle.velocity = velocity
 
       particle.radius = Random.floatBetween(0, settings.particles.maxRadius)
     }
 
-		if (settings.staticParticles) {
-			for (const coords of settings.staticParticles) {
-				const p = this._particleManager.createParticle()
-				p.active = false
-				p.radius = 0
-				p.position.set(settings.renderer.width * coords[0], settings.renderer.height * coords[1])
-			}
-		}
+    if (settings.staticParticles) {
+      for (const coords of settings.staticParticles) {
+        const p = this._particleManager.createParticle()
+        p.active = false
+        p.radius = 0
+        p.position.set(
+          settings.renderer.width * coords[0],
+          settings.renderer.height * coords[1]
+        )
+      }
+    }
 
-		this._renderer = new Renderer(this._ctx, settings.renderer.backgroundColor)
+    if (settings.renderer.dpiMultiplier) {
+      this._dpiMultiplier = settings.renderer.dpiMultiplier
+    } else {
+      this._dpiMultiplier = 2
+    }
 
-		this._renderer.transparentBackground = settings.renderer.transparentBackground
+    this._renderer = new Renderer(
+      this._ctx,
+      settings.renderer.backgroundColor,
+      new Vector2(),
+      this._dpiMultiplier
+    )
 
-		this._setSize(settings.renderer.width, settings.renderer.height)
+    this._renderer.transparentBackground =
+      settings.renderer.transparentBackground
 
-		if (settings.renderer.linearGradient) {
-			const gradient = this._ctx.createLinearGradient(
-				settings.renderer.width * settings.renderer.linearGradient.x1,
-				settings.renderer.height * settings.renderer.linearGradient.y1,
-				settings.renderer.width * settings.renderer.linearGradient.x2,
-				settings.renderer.height * settings.renderer.linearGradient.y2,
-			);
-			gradient.addColorStop(0, settings.renderer.linearGradient.color1);
-			gradient.addColorStop(1, settings.renderer.linearGradient.color2);
-			this._renderer.gradient = gradient;
-		}
+    this._setSize(settings.renderer.width, settings.renderer.height)
 
-		this.debug = settings.debug
+    if (settings.renderer.linearGradient) {
+      const gradient = this._ctx.createLinearGradient(
+        settings.renderer.width *
+          settings.renderer.linearGradient.x1 *
+          this._dpiMultiplier,
+        settings.renderer.height *
+          settings.renderer.linearGradient.y1 *
+          this._dpiMultiplier,
+        settings.renderer.width *
+          settings.renderer.linearGradient.x2 *
+          this._dpiMultiplier,
+        settings.renderer.height *
+          settings.renderer.linearGradient.y2 *
+          this._dpiMultiplier
+      )
+      gradient.addColorStop(0, settings.renderer.linearGradient.color1)
+      gradient.addColorStop(1, settings.renderer.linearGradient.color2)
+      this._renderer.gradient = gradient
+    }
 
-		if (settings.resize) {
-			window.addEventListener('resize', this._onResize.bind(this))
-		}
+    this.debug = settings.debug
 
-		return this
-	}
+    if (settings.resize) {
+      window.addEventListener('resize', this._onResize.bind(this))
+    }
 
-	_onResize() {
-		if (this._resizeTimeout) {
-			clearTimeout(this._resizeTimeout)
-		}
-		this._resizeTimeout = setTimeout(() => this._setSize(window.innerWidth, window.innerHeight), 100)
-	}
+    return this
+  }
 
-	start() {
-		this._isRunning = true
-		window.requestAnimationFrame(this._boundUpdate)
-		
-		console.info('Spark Partilces started!')
-		return this
-	}
+  _onResize() {
+    if (this._resizeTimeout) {
+      clearTimeout(this._resizeTimeout)
+    }
+    this._resizeTimeout = setTimeout(
+      () => this._setSize(window.innerWidth, window.innerHeight),
+      100
+    )
+  }
 
-	stop() {
-		this._isRunning = false
-		return this
-	}
+  start() {
+    this._isRunning = true
+    window.requestAnimationFrame(this._boundUpdate)
 
-	_update() {
-		const startTime = Date.now()
+    console.info('Spark Partilces started!')
+    return this
+  }
 
-		this._quadtree = new QuadTree(this._boundary, 4)
-		const activeParticles = this._particleManager.particles.filter((p) => p.active)
-		for (const particle of activeParticles) {
-			particle.update()
+  stop() {
+    this._isRunning = false
+    return this
+  }
 
-			this._checkBoundary(particle, this._boundary)
-			this._quadtree.insert(particle)
-		}
+  _update() {
+    const startTime = Date.now()
 
-		let lines = []
-		if (this._settings.particles.linkedParticles) {
-			lines = this._linkPartiles(this._particleManager.particles, this._settings.particles.distanceToLink)
-		}
+    this._quadtree = new QuadTree(this._boundary, 4)
+    const activeParticles = this._particleManager.particles.filter(
+      (p) => p.active
+    )
+    for (const particle of activeParticles) {
+      particle.update()
 
-		this._renderer.particles = this._particleManager.particles
-		this._renderer.lines = lines
-		this._renderer.deltas = this._deltas
-		this._renderer.render()
+      this._checkBoundary(particle, this._boundary)
+      this._quadtree.insert(particle)
+    }
 
-		if (this._isRunning) {
-			window.requestAnimationFrame(this._boundUpdate)
-		}
+    let lines = []
+    if (this._settings.particles.linkedParticles) {
+      lines = this._linkPartiles(
+        this._particleManager.particles,
+        this._settings.particles.distanceToLink
+      )
+    }
 
-		this._ticks++
+    this._renderer.particles = this._particleManager.particles
+    this._renderer.lines = lines
+    this._renderer.deltas = this._deltas
+    this._renderer.render()
 
-		const endTime = Date.now()
-		const delta = endTime - startTime
-		this._deltas.push(delta)
-		if (this._deltas.length > this._deltas.length - 1) {
-			this._deltas.shift()
-		}
-		const a = this._deltas.filter((v) => v !== undefined)
+    if (this._isRunning) {
+      window.requestAnimationFrame(this._boundUpdate)
+    }
 
-		return this
-	}
+    this._ticks++
 
-	_linkPartiles(particles, distanceToLink) {
-		const lines = []
+    const endTime = Date.now()
+    const delta = endTime - startTime
+    this._deltas.push(delta)
+    if (this._deltas.length > this._deltas.length - 1) {
+      this._deltas.shift()
+    }
+    const a = this._deltas.filter((v) => v !== undefined)
 
-		for (let a = 0; a < particles.length - 1; a++) {
-			for (let b = a + 1; b < particles.length; b++) {
-				const distance = particles[a].position.distance(particles[b].position)
-				if (distance < distanceToLink) {
-					const line = new Line(
-						Vector2.fromVector(particles[a].position),
-						Vector2.fromVector(particles[b].position)
-					)
-					const alpha = 1 - distance / distanceToLink
-					line.alpha = alpha
-					lines.push(line)
-				}
-			}
-		}
+    return this
+  }
 
-		// for (const particleA of particles) {
-		// 	const boundCircle = new Circle(particleA.position.x, particleA.position.y, distanceToLink)
-		// 	const inBoundParticles = this._quadtree.queryCircle(boundCircle)
+  _linkPartiles(particles, distanceToLink) {
+    const lines = []
 
-		// 	for (const particleB of inBoundParticles) {
-		// 		if (particleA === particleB) {
-		// 			continue
-		// 		}
+    for (let a = 0; a < particles.length - 1; a++) {
+      for (let b = a + 1; b < particles.length; b++) {
+        const distance = particles[a].position.distance(particles[b].position)
+        if (distance < distanceToLink) {
+          const line = new Line(
+            Vector2.fromVector(particles[a].position),
+            Vector2.fromVector(particles[b].position)
+          )
+          const alpha = 1 - distance / distanceToLink
+          line.alpha = alpha
+          lines.push(line)
+        }
+      }
+    }
 
-		// 		const distance = particleA.position.distance(particleB.position)
-		// 		const line = new Line(
-		// 			Vector2.fromVector(particleA.position),
-		// 			Vector2.fromVector(particleB.position)
-		// 		)
-		// 		const alpha = 1 - distance / distanceToLink
-		// 		line.alpha = alpha
-		// 		lines.push(line)
-		// 	}
-		// }
+    // for (const particleA of particles) {
+    // 	const boundCircle = new Circle(particleA.position.x, particleA.position.y, distanceToLink)
+    // 	const inBoundParticles = this._quadtree.queryCircle(boundCircle)
 
-		return lines
-	}
+    // 	for (const particleB of inBoundParticles) {
+    // 		if (particleA === particleB) {
+    // 			continue
+    // 		}
 
-	oneFrame = this._update
+    // 		const distance = particleA.position.distance(particleB.position)
+    // 		const line = new Line(
+    // 			Vector2.fromVector(particleA.position),
+    // 			Vector2.fromVector(particleB.position)
+    // 		)
+    // 		const alpha = 1 - distance / distanceToLink
+    // 		line.alpha = alpha
+    // 		lines.push(line)
+    // 	}
+    // }
 
-	_setSize(width, height) {
-		this._canvas.width = width
-		this._canvas.height = height
+    return lines
+  }
 
-		this._renderer.viewportSize = new Vector2(width, height)
-		this._boundary = new Rectangle(
-			-this._settings.particles.distanceToLink,
-			-this._settings.particles.distanceToLink,
-			width + this._settings.particles.distanceToLink * 2,
-			height + this._settings.particles.distanceToLink * 2
-		)
-		this._quadtree = new QuadTree(this._boundary, 4)
+  oneFrame = this._update
 
-		return this
-	}
+  _setSize(width, height) {
+    this._canvas.style.width = width.toString() + 'px'
+    this._canvas.style.height = height.toString() + 'px'
 
-	get debug() {
-		return this._debug
-	}
+    this._canvas.width = width * this._dpiMultiplier
+    this._canvas.height = height * this._dpiMultiplier
 
-	set debug(v) {
-		this._debug = v
-		this._renderer._debug = v
-	}
+    this._renderer.viewportSize = new Vector2(width, height)
 
-	_checkBoundary(particle, boundary) {
-		if (particle.position.x < boundary.left) {
-			particle.position.x = boundary.right
-			return
-		}
+    this._boundary = new Rectangle(
+      -this._settings.particles.distanceToLink,
+      -this._settings.particles.distanceToLink,
+      width + this._settings.particles.distanceToLink * 2,
+      height + this._settings.particles.distanceToLink * 2
+    )
+    this._quadtree = new QuadTree(this._boundary, 4)
 
-		if (particle.position.x > boundary.right) {
-			particle.position.x = boundary.left
-			return
-		}
+    return this
+  }
 
-		if (particle.position.y < boundary.top) {
-			particle.position.y = boundary.bottom
-			return
-		}
+  get debug() {
+    return this._debug
+  }
 
-		if (particle.position.y > boundary.bottom) {
-			particle.position.y = boundary.top
-		}
-	}
+  set debug(v) {
+    this._debug = v
+    this._renderer._debug = v
+  }
+
+  _checkBoundary(particle, boundary) {
+    if (particle.position.x < boundary.left) {
+      particle.position.x = boundary.right
+      return
+    }
+
+    if (particle.position.x > boundary.right) {
+      particle.position.x = boundary.left
+      return
+    }
+
+    if (particle.position.y < boundary.top) {
+      particle.position.y = boundary.bottom
+      return
+    }
+
+    if (particle.position.y > boundary.bottom) {
+      particle.position.y = boundary.top
+    }
+  }
 }
--- src/Renderer.js
+++ src/Renderer.js
@@ -1,7 +1,12 @@
-import Vector2 from "./Vector2.js"
+import Vector2 from './Vector2.js'
 
 export default class Renderer {
-  constructor(ctx, backgroundColor = '#000', viewportSize = new Vector2(300, 150)) {
+  constructor(
+    ctx,
+    backgroundColor = '#000',
+    viewportSize = new Vector2(300, 150),
+    dpiMultiplier = 1
+  ) {
     this.particles = []
 
     this.lines = []
@@ -9,6 +14,8 @@ export default class Renderer {
     this.deltas = []
 
     this.gradient = null
+
+    this._dpiMultiplier = dpiMultiplier
 
     this._ctx = ctx
 
@@ -19,8 +26,6 @@ export default class Renderer {
     this.transparentBackground = true
 
     this._backgroundColor = backgroundColor
-
-    this._ctx.lineWidth = 1
   }
 
   set viewportSize(value) {
@@ -29,51 +34,80 @@ export default class Renderer {
 
   render() {
     if (this.transparentBackground) {
-      this._ctx.clearRect(0, 0, this._viewportSize.x, this._viewportSize.y)
+      this._ctx.clearRect(
+        0,
+        0,
+        this._viewportSize.x * this._dpiMultiplier,
+        this._viewportSize.y * this._dpiMultiplier
+      )
     } else if (this.gradient) {
       this._ctx.fillStyle = this.gradient
-      this._ctx.fillRect(0, 0, this._viewportSize.x, this._viewportSize.y)
+      this._ctx.fillRect(
+        0,
+        0,
+        this._viewportSize.x * this._dpiMultiplier,
+        this._viewportSize.y * this._dpiMultiplier
+      )
     } else {
       this._ctx.fillStyle = this._backgroundColor
-      this._ctx.fillRect(0, 0, this._viewportSize.x, this._viewportSize.y)
+      this._ctx.fillRect(
+        0,
+        0,
+        this._viewportSize.x * this._dpiMultiplier,
+        this._viewportSize.y * this._dpiMultiplier
+      )
     }
 
     const particlesWithLetter = this.particles.filter((p) => Boolean(p.letter))
-    const particlesWithoutLetter = this.particles.filter((p) => !Boolean(p.letter))
+    const particlesWithoutLetter = this.particles.filter(
+      (p) => !Boolean(p.letter)
+    )
 
     this._ctx.fillStyle = `rgba(255,255,255,0.7)`
-    this._ctx.font = '30px monospace'
+    this._ctx.font = `${30 * this._dpiMultiplier}px monospace`
     for (const particle of particlesWithLetter) {
-      this._ctx.fillText(particle.letter, particle.position.x - 8, particle.position.y + 10)
+      this._ctx.fillText(
+        particle.letter,
+        (particle.position.x - 8) * this._dpiMultiplier,
+        (particle.position.y + 10) * this._dpiMultiplier
+      )
     }
-
 
     this._ctx.fillStyle = `rgba(255,255,255,0.5)`
     this._ctx.beginPath()
     for (const particle of particlesWithoutLetter) {
-      this._ctx.moveTo(particle.position.x, particle.position.y)
-      this._ctx.arc(particle.position.x, particle.position.y, particle.radius, 0, Math.PI * 2)
+      this._ctx.moveTo(
+        particle.position.x * this._dpiMultiplier,
+        particle.position.y * this._dpiMultiplier
+      )
+      this._ctx.arc(
+        particle.position.x * this._dpiMultiplier,
+        particle.position.y * this._dpiMultiplier,
+        particle.radius * this._dpiMultiplier,
+        0,
+        Math.PI * 2
+      )
     }
     this._ctx.fill()
 
+    this._ctx.lineWidth = 1 * this._dpiMultiplier
     for (const line of this.lines) {
       this._ctx.beginPath()
       this._ctx.strokeStyle = `rgba(255,255,255,${line.alpha})`
-      this._ctx.moveTo(line.a.x, line.a.y)
-      this._ctx.lineTo(line.b.x, line.b.y)
+      this._ctx.moveTo(
+        line.a.x * this._dpiMultiplier,
+        line.a.y * this._dpiMultiplier
+      )
+      this._ctx.lineTo(
+        line.b.x * this._dpiMultiplier,
+        line.b.y * this._dpiMultiplier
+      )
       this._ctx.stroke()
     }
 
     if (this._debug) {
       this.drawPerformanceGraphic()
     }
-  }
-
-  drawCircle(ctx, x, y, radius, color) {
-    ctx.fillStyle = color
-    ctx.beginPath()
-    ctx.arc(x, y, radius, 0, Math.PI * 2)
-    ctx.fill()
   }
 
   drawPerformanceGraphic() {
@@ -83,7 +117,7 @@ export default class Renderer {
     const min = Math.min(...raw)
     const max = Math.max(...raw)
 
-    this._ctx.font = '16px monospace'
+    this._ctx.font = `${16 * this._dpiMultiplier}px monospace`
     this._ctx.strokeStyle = `#fff`
     this._ctx.fillStyle = `#fff`
 
@@ -91,25 +125,53 @@ export default class Renderer {
     const startY = 70
 
     this._ctx.beginPath()
-    this._ctx.moveTo(offsetX, startY - this.deltas[0])
+    this._ctx.moveTo(
+      offsetX * this._dpiMultiplier,
+      (startY - this.deltas[0]) * this._dpiMultiplier
+    )
     for (const delta of this.deltas) {
       const offsetY = startY - 50 * normalize(delta, min, max)
-      this._ctx.lineTo(offsetX, offsetY)
+      this._ctx.lineTo(
+        offsetX * this._dpiMultiplier,
+        offsetY * this._dpiMultiplier
+      )
       offsetX += 1
     }
 
-    this._ctx.moveTo(0, startY)
-    this._ctx.lineTo(this.deltas.length, startY)
-    this._ctx.fillText(min, this.deltas.length, startY + 4)
+    this._ctx.moveTo(0, startY * this._dpiMultiplier)
+    this._ctx.lineTo(
+      this.deltas.length * this._dpiMultiplier,
+      startY * this._dpiMultiplier
+    )
+    this._ctx.fillText(
+      min,
+      this.deltas.length * this._dpiMultiplier,
+      (startY + 4) * this._dpiMultiplier
+    )
 
-    this._ctx.moveTo(0, startY - 50)
-    this._ctx.lineTo(this.deltas.length, startY - 50)
-    this._ctx.fillText(max, this.deltas.length, startY - 50 + 4)
+    this._ctx.moveTo(0, (startY - 50) * this._dpiMultiplier)
+    this._ctx.lineTo(
+      this.deltas.length * this._dpiMultiplier,
+      (startY - 50) * this._dpiMultiplier
+    )
+    this._ctx.fillText(
+      max,
+      this.deltas.length * this._dpiMultiplier,
+      (startY - 50 + 4) * this._dpiMultiplier
+    )
 
     this._ctx.closePath()
     this._ctx.stroke()
 
-    this._ctx.fillText(`${this.particles.length.toString()} particles`, 0, 15)
-    this._ctx.fillText(`${this.deltas[this.deltas.length - 1]?.toString()} ms`, 130, 15)
+    this._ctx.fillText(
+      `${this.particles.length.toString()} particles`,
+      0,
+      15 * this._dpiMultiplier
+    )
+    this._ctx.fillText(
+      `${this.deltas[this.deltas.length - 1]?.toString()} ms`,
+      130 * this._dpiMultiplier,
+      15 * this._dpiMultiplier
+    )
   }
 }
