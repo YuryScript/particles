e0cdec50680150616d30000ba0df70afc2c88dc0
Yury Petrov <yuratorn@gmail.com>
2021-04-02 22:52:04 +0300

    prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
